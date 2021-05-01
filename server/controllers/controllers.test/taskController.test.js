48694d207a9bf0b07cd523adf290e3077666fe79
Simen H <simenmelo@gmail.com>
2021-05-01 14:45:34 +0200

    test: create test for adding existing tasks

--- server/controllers/controllers.test/taskController.test.js
+++ server/controllers/controllers.test/taskController.test.js
@@ -9,16 +9,18 @@ const Task = require('../../models/task');
 const mongoose = require('mongoose');
 const databaseName = 'test';
 
+const app = express();
+app.use(express.json());
+app.use(router);
+const request = supertest(app);
+
+const mockUser = { firstName: 'John', lastName: 'Doe', email: 'email@email.com', password: 'password' };
+let userId;
+let listId;
+let sectionId;
+let taskId;
+
 describe('Adding new tasks', () => {
-  const app = express();
-  app.use(express.json());
-  app.use(router);
-  const request = supertest(app);
-
-  const mockUser = { firstName: 'John', lastName: 'Doe', email: 'email@email.com', password: 'password' };
-  let userId;
-  let listId;
-
   beforeAll(async () => {
     const url = `mongodb://127.0.0.1/${databaseName}`;
     await mongoose.connect(url, { useNewUrlParser: true });
@@ -40,8 +42,8 @@ describe('Adding new tasks', () => {
   });
 
   it('should save a task to the database', async done => {
-    const thisList = await List.findById(listId);
-    const sectionId = thisList.sections[0]._id;
+    const curList = await List.findById(listId);
+    const sectionId = curList.sections[0]._id;
 
     const title1 = 'This is a test task';
     const title2 = 'This is another test task';
@@ -61,14 +63,56 @@ describe('Adding new tasks', () => {
   });
 
   it('should not save a task to the database if no title is passed in the body', async done => {
-    const thisList = await List.findById(listId);
-    const sectionId = thisList.sections[0]._id;
+    const curList = await List.findById(listId);
+    const sectionId = curList.sections[0]._id;
 
     const res = await request.post(`/users/${userId}/lists/${listId}/sections/${sectionId}/tasks`);
 
     const tasks = await Task.find();
     expect(res.status).toBe(500);
     expect(tasks).toHaveLength(0);
+    done();
+  });
+});
+
+describe('Adding existing tasks', () => {
+  beforeAll(async () => {
+    const url = `mongodb://127.0.0.1/${databaseName}`;
+    await mongoose.connect(url, { useNewUrlParser: true });
+  });
+
+  beforeEach(async () => {
+    userId = await request.post('/users').send(mockUser);
+    userId = userId.body._id;
+    listId = await request.post(`/users/${userId}/lists`).send({ title: 'title' });
+    listId = listId.body[0]._id;
+    const curList = await List.findById(listId);
+    sectionId = curList.sections[0]._id;
+    taskId = await request
+      .post(`/users/${userId}/lists/${listId}/sections/${sectionId}/tasks`)
+      .send({ title: 'title' });
+    taskId = taskId.body[0]._id;
+  });
+
+  afterEach(async () => {
+    await User.deleteMany();
+    await List.deleteMany();
+    await Task.deleteMany();
+  });
+
+  it('should add an existing task to a new section', async done => {
+    const temp = await request
+      .post(`/users/${userId}/lists/${listId}/sections`)
+      .send({ title: 'New section' });
+    const newSection = temp.body[0].sections[1];
+
+    const res = await request
+      .put(`/users/${userId}/lists/${listId}/sections/${newSection._id}/tasks`)
+      .send({ taskId });
+
+    const curList = await List.findById(listId);
+    const receivedTaskId = curList.sections[1].tasks[0]._id.toString();
+    expect(receivedTaskId).toStrictEqual(taskId);
     done();
   });
 });
