354d7498c672867564dd17a8bab7a45f74d48dd5
Matt Srotyr <srotyr@gmx.com>
2021-05-03 12:21:48 +0100

    Add test for userLogin

--- server/controllers/controllers.test/userController.test.js
+++ server/controllers/controllers.test/userController.test.js
@@ -13,6 +13,7 @@ app.use(router);
 const request = supertest(app);
 
 describe('User controller tests', () => {
+  const mockUser = { firstName: 'John', lastName: 'Doe', email: 'email@email.com', password: 'password' };
   beforeAll(async () => {
     const url = `mongodb://127.0.0.1/${databaseName}`;
     await mongoose.connect(url, { useNewUrlParser: true });
@@ -22,9 +23,11 @@ describe('User controller tests', () => {
     await User.deleteMany();
   });
 
-  it('should create and save a new user to the database', async done => {
-    const mockUser = { firstName: 'John', lastName: 'Doe', email: 'email@email.com', password: 'password' };
+  afterAll(async () => {
+    await mongoose.connection.close();
+  })
 
+  it('should create and save a new user to the database', async () => {
     await request.post('/users/').send(mockUser);
 
     const user1 = await User.findOne({ firstName: mockUser.firstName });
@@ -33,6 +36,18 @@ describe('User controller tests', () => {
     expect(user1.lastName).toBe(mockUser.lastName);
     expect(user1.email).toBe(mockUser.email);
     expect(user1.password).toBe(mockUser.password);
-    done();
   });
+
+  it('Should login the user if the user exists', async () => {
+    await User.create(mockUser)
+    user = await User.find({email: mockUser.email})
+    const {body, status} = await request.post('/users/login')
+      .send({email: mockUser.email, password: mockUser.password})
+      expect(body).toEqual(expect.objectContaining(
+        {
+          _id: expect.any(String),
+          message: 'Successfully Logged in user'
+        }
+      ));
+  })
 });
