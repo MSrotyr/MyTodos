2aef0321956c431c7c5a6ffeb1b3edcb6d250c20
Simen H <simenmelo@gmail.com>
2021-05-03 12:24:11 +0200

    chore: provide context to lists with userId

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
 import { AllLists } from './Containers/AllLists/AllLists';
 import { SelectedList } from './Containers/SelectedList/SelectedList';
 import './App.css';
 
+const UserIdContext = createContext(null);
 function App() {
+  const [userId, setUserId] = useState(''); // if user id is empty, render login/register page
   return (
     <div className="App">
       <main className="main">
-        <AllLists />
-        <SelectedList />
+        <UserIdContext.Provider value={{
+          userId,
+          setUserId: (id) => setUserId(id),
+        }}
+        >
+          <AllLists />
+          <SelectedList />
+        </UserIdContext.Provider>
       </main>
       <footer />
     </div>
